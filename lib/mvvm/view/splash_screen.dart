80033d14484d43c76c298c0f4ee9c6fbc127f6b2
Samundra Bhandari <76542341+Samundra-github@users.noreply.github.com>
2023-06-16 14:32:12 +0545

    Day 11:- Lottie animation as a Splash Screen

--- lib/mvvm/view/splash_screen.dart
+++ lib/mvvm/view/splash_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/mvvm/data/app_exception.dart';
-import 'package:flutter_application_1/mvvm/utils/utils.dart';
+// import 'package:flutter_application_1/mvvm/data/app_exception.dart';
+// import 'package:flutter_application_1/mvvm/utils/utils.dart';
 import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
+
+import '../res/routes/routes_name.dart';
 // import 'package:flutter_application_1/mvvm/res/routes/routes_name.dart';
 // import 'package:get/get.dart';
 
@@ -16,21 +19,34 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('appBar'.tr),
-      ),
+      // appBar: AppBar(
+      //   centerTitle: true,
+      //   title: Text('appBar'.tr),
+      // ),
+      backgroundColor: Colors.white,
       body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              // Get.toNamed(RouteName.loginScreen);
-              Utils.toastMessage('Hello Samundra');
-            },
-            child: const Text('Go TO Login')),
+        // child: ElevatedButton(
+        //     onPressed: () {
+        //       // Get.toNamed(RouteName.loginScreen);
+        //       Utils.toastMessage('Hello Samundra');
+        //     },
+        //     child: const Text('Go TO Login')),
+        child: Lottie.asset(
+          'assets/icons/ground.json',
+          height: 500,
+          width: Get.width * 1,
+          repeat: false,
+          animate: true,
+          onLoaded: (composition) {
+            Future.delayed(const Duration(seconds: 5), () {
+              Get.toNamed(RouteName.loginScreen);
+            });
+          },
+        ),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        throw ServerException('Server Failed');
-      }),
+      // floatingActionButton: FloatingActionButton(onPressed: () {
+      //   throw ServerException('Server Failed');
+      // }),
     );
   }
 }
